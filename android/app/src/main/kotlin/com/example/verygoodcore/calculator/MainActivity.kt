939d17585022d8bccf652338c9a9ed1e95cb6d7a
Dongmin Yu <ydm2790@gmail.com>
2024-12-31 12:29:33 +0900

    chore: 🧹 clean up configuration files and improve formatting

--- android/app/src/main/kotlin/com/example/verygoodcore/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verygoodcore/calculator/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.verygoodcore.calculator
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
