66df7e3e81c0d1fa3fb44c8c0d528df6e4df5079
Dongmin Yu <ydm2790@gmail.com>
2025-01-01 14:36:27 +0900

    fix: ✅ update test to render CalculatorScreen instead of CounterPage

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -3,13 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 
 // 🌎 Project imports:
 import 'package:calculator/app/app.dart';
-import 'package:calculator/features/counter/counter.dart';
+import 'package:calculator/features/calculator/presentation/pages/calculator_screen.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (WidgetTester tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(CalculatorScreen), findsOneWidget);
     });
   });
 }
