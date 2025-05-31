751dfb8cf99041459114b1256cd644df9cd44c49
Dongmin Yu <ydm2790@gmail.com>
2025-05-31 21:22:01 +0900

    test: 🧪 add widget tests for CalculatorScreen interactions
    This commit adds widget tests to verify the interactions with the CalculatorScreen.
    
    - Adds tests to ensure that button presses trigger the correct events and update the display accordingly.
    - Covers scenarios for number input, operator selection, result calculation, and clearing the display.
    - Includes tests for delete and flipSign buttons.
    - Adds tests to cover the remaining buttons and clear functionality.
    - Introduces a `tapButton` extension method on `WidgetTester` to simplify button tapping in tests.

--- test/presentation/pages/calculator_screen_test.dart
+++ test/presentation/pages/calculator_screen_test.dart
@@ -8,7 +8,9 @@ import 'package:flutter_test/flutter_test.dart';
 // 🌎 Project imports:
 import 'package:calculator/data/repositories/calculate_repository_impl.dart';
 import 'package:calculator/presentation/bloc/calculator_bloc.dart';
+import 'package:calculator/presentation/enums/button_type.dart';
 import 'package:calculator/presentation/pages/calculator_screen.dart';
+import 'package:calculator/presentation/widgets/calculator_button.dart';
 import '../../calculator_test.mocks.dart';
 
 void main() {
@@ -65,4 +67,156 @@ void main() {
     await tester.pumpWidget(const MaterialApp(home: SizedBox()));
     expect(find.byType(CalculatorScreen), findsNothing);
   });
+
+  testWidgets('CalculatorScreen buttonPressed triggers correct events', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<CalculatorBloc>(
+          create: (context) => CalculatorBloc(
+            repository: CalculatorRepositoryImpl(
+              localDatasource: mockLocalDatasource,
+              remoteDatasource: mockRemoteDatasource,
+              connectivity: mockConnectivity,
+            ),
+          ),
+          child: const CalculatorView(),
+        ),
+      ),
+    );
+
+    // [1] 숫자 버튼을 누르고 식에 반영되는지 확인
+    await tester.tapButton(ButtonType.one);
+
+    // [2] 연산자 버튼을 누르고 식에 반영되는지 확인
+    await tester.tapButton(ButtonType.plus);
+
+    // [3] 또 다른 숫자 버튼 누름
+    await tester.tapButton(ButtonType.two);
+
+    // [4] '=' 버튼(결과 버튼) 누름
+    await tester.tapButton(ButtonType.result);
+
+    // [5] 결과가 정상적으로 표시되는지 확인
+    expect(find.text('3'), findsWidgets); // '결과' 텍스트가 화면에 노출되는지 체크
+
+    // [6] AC(초기화) 버튼도 누르고 결과가 '0'이 되는지 확인
+    await tester.tapButton(ButtonType.clear);
+
+    expect(find.text('0'), findsWidgets); // 초기화 후 결과가 0인지 체크
+  });
+
+  testWidgets('CalculatorScreen buttonPressed for delete and flipSign', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<CalculatorBloc>(
+          create: (context) => CalculatorBloc(
+            repository: CalculatorRepositoryImpl(
+              localDatasource: mockLocalDatasource,
+              remoteDatasource: mockRemoteDatasource,
+              connectivity: mockConnectivity,
+            ),
+          ),
+          child: const CalculatorView(),
+        ),
+      ),
+    );
+
+    // 숫자 '7' 입력
+    await tester.tapButton(ButtonType.seven);
+
+    // FlipSign(+/-) 버튼 누름
+    await tester.tapButton(ButtonType.plusMinus);
+
+    // empty 버튼 누름 (아무 기능도 하지 않음)
+    await tester.tapButton(ButtonType.empty);
+
+    // 음수로 바뀌었는지 확인
+    expect(find.text('-7'), findsWidgets);
+
+    // Delete 버튼 누름
+    final deleteOrClearButtonFinder = find.byWidgetPredicate(
+      (widget) => widget is CalculatorButton && (widget.button == ButtonType.delete),
+    );
+    expect(deleteOrClearButtonFinder, findsOneWidget);
+    await tester.ensureVisible(deleteOrClearButtonFinder);
+    await tester.tap(deleteOrClearButtonFinder);
+    await tester.pumpAndSettle();
+
+    // 결과가 '0'인지 확인
+    expect(find.text('0'), findsWidgets);
+  });
+
+  testWidgets('Press the remaining buttons and clear.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<CalculatorBloc>(
+          create: (context) => CalculatorBloc(
+            repository: CalculatorRepositoryImpl(
+              localDatasource: mockLocalDatasource,
+              remoteDatasource: mockRemoteDatasource,
+              connectivity: mockConnectivity,
+            ),
+          ),
+          child: const CalculatorView(),
+        ),
+      ),
+    );
+
+    // 예시: 45698 * 0 =
+    final sequence = <ButtonType>[
+      ButtonType.four,
+      ButtonType.five,
+      ButtonType.six,
+      ButtonType.nine,
+      ButtonType.eight,
+      ButtonType.multiple,
+      ButtonType.zero,
+      ButtonType.result,
+    ];
+
+    for (final buttonType in sequence) {
+      await tester.tapButton(buttonType);
+    }
+
+    // 결과가 0인지 확인
+    expect(find.text('0'), findsWidgets);
+
+    // 예시: 0.3 / 0.3 =
+    final sequence2 = <ButtonType>[
+      ButtonType.zero,
+      ButtonType.dot,
+      ButtonType.three,
+      ButtonType.division,
+      ButtonType.three,
+      ButtonType.zero,
+      ButtonType.percent,
+      ButtonType.result,
+    ];
+
+    for (final buttonType in sequence2) {
+      await tester.tapButton(buttonType);
+    }
+
+    // 결과가 1인지 확인
+    expect(find.text('1'), findsWidgets);
+
+    // 마지막에 clear(AC)로 초기화
+    await tester.tapButton(ButtonType.minus);
+    await tester.tapButton(ButtonType.clear);
+
+    // 결과가 0인지 확인
+    expect(find.text('0'), findsWidgets);
+  });
+}
+
+extension _CalculatorInteraction on WidgetTester {
+  Future<void> tapButton(ButtonType buttonType) async {
+    final finder = find.byWidgetPredicate(
+      (widget) => widget is CalculatorButton && widget.button == buttonType,
+    );
+    expect(finder, findsOneWidget, reason: '$buttonType should exist');
+    await ensureVisible(finder);
+    await tap(finder);
+    await pumpAndSettle();
+  }
 }
